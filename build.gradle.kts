f33cae69624c3d10056edf417f6e2ef88a57dd7d
hyunmin <dndb4902@krafton.com>
2025-02-18 09:50:49 +0900

    chore: cache dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// cache
+	implementation("org.springframework.boot:spring-boot-starter-cache:3.4.2")
+	implementation("org.ehcache:ehcache:3.10.8")
 }
 
 kotlin {
